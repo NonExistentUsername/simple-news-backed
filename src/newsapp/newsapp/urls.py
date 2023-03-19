939b871df6e9d93c285412e191802298453f2744
dmytromayor <mayor4090@gmail.com>
2023-03-19 03:14:27 +0200

    Add news django app

--- src/newsapp/newsapp/urls.py
+++ src/newsapp/newsapp/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from news.urls import urlpatterns as news_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("news/", include(news_urlpatterns)),
 ]
 
 if settings.DEBUG:
