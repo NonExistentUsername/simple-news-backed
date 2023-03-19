bf70032d4b3293eb3e7cc840c5291e881563a15c
dmytromayor <mayor4090@gmail.com>
2023-03-19 02:55:45 +0200

    Setup django project

---
+++ src/newsapp/newsapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns.append(*static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
