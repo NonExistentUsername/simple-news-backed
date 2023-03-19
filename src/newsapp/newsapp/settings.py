b72799036a87f35ef9cd7e6a0f6f0f4b1e592e31
dmytromayor <mayor4090@gmail.com>
2023-03-19 03:16:10 +0200

    Fix redis hostname

--- src/newsapp/newsapp/settings.py
+++ src/newsapp/newsapp/settings.py
@@ -97,8 +97,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # celery
 
-CELERY_BROKER_URL = "redis://newsapp:6379"
-CELERY_RESULT_BACKEND = "redis://newsapp:6379"
+CELERY_BROKER_URL = "redis://newsapp-redis:6379"
+CELERY_RESULT_BACKEND = "redis://newsapp-redis:6379"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
