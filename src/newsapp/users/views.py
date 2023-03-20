bb178be8c82296e064bc9ca131ab59d9034b42dc
dmytromayor <mayor4090@gmail.com>
2023-03-20 22:55:35 +0200

    Update usersview

--- src/newsapp/users/views.py
+++ src/newsapp/users/views.py
@@ -8,21 +8,6 @@ from users.serializers import UserSerializer
 class UserView(
     viewsets.GenericViewSet,
     mixins.CreateModelMixin,
-    mixins.UpdateModelMixin,
 ):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-
-    def update(self, request, *args, **kwargs):
-        if self.request.user.is_staff:
-            return super().update(request, *args, **kwargs)
-
-        if self.request.user.is_anonymous:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        object: User = self.get_object()
-
-        if object != self.request.user:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        return super().update(request, *args, **kwargs)
