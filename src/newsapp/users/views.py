1b1bff293f14644eb55544afe78b9990a037379a
dmytromayor <mayor4090@gmail.com>
2023-03-20 22:54:40 +0200

    Update usersview

--- src/newsapp/users/views.py
+++ src/newsapp/users/views.py
@@ -1,12 +1,28 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework import generics, mixins, views, viewsets
+from rest_framework import generics, mixins, status, views, viewsets
+from rest_framework.response import Response
 from users.serializers import UserSerializer
 
 
 class UserView(
     viewsets.GenericViewSet,
     mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
 ):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        if self.request.user.is_staff:
+            return super().update(request, *args, **kwargs)
+
+        if self.request.user.is_anonymous:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        object: User = self.get_object()
+
+        if object != self.request.user:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return super().update(request, *args, **kwargs)
