e4c335cffdf836b61a097c799ac672fd9b00a835
dmytromayor <mayor4090@gmail.com>
2023-03-20 21:31:15 +0200

    Add news view to urls

--- src/newsapp/news/urls.py
+++ src/newsapp/news/urls.py
@@ -4,5 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from news.views import NewsView
+from rest_framework import routers
 
-urlpatterns: t.List[t.Any] = []
+router = routers.DefaultRouter()
+router.register(r"news", NewsView)
+
+urlpatterns: t.List[t.Any] = [] + router.urls
