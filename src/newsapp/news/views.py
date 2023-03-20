d71c23a67f0a2cbb9e014e45a10a78068caef447
dmytromayor <mayor4090@gmail.com>
2023-03-20 21:31:01 +0200

    Add view for news

--- src/newsapp/news/views.py
+++ src/newsapp/news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from news.models import News
+from news.serializers import NewsSerializer
+from rest_framework import mixins, views, viewsets
 
-# Create your views here.
+
+class NewsView(
+    viewsets.GenericViewSet,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+):
+    serializer_class = NewsSerializer
+    queryset = News.objects.all()
