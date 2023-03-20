07c789a5882244d432967a78ac56389d0ece3055
dmytromayor <mayor4090@gmail.com>
2023-03-20 21:21:26 +0200

    Enable admin site only in debug mode

--- src/newsapp/newsapp/urls.py
+++ src/newsapp/newsapp/urls.py
@@ -6,11 +6,11 @@ from news.urls import urlpatterns as news_urlpatterns
 from users.urls import urlpatterns as users_urlpatterns
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/news/", include(news_urlpatterns)),
     path("api/auth/", include(users_urlpatterns)),
 ]
 
 if settings.DEBUG:
+    urlpatterns.append(path("admin/", admin.site.urls))
     urlpatterns.append(*static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
