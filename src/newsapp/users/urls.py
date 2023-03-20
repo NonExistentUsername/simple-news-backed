0871f211cfb678ab654631f2f558a8ee8caa8e6a
dmytromayor <mayor4090@gmail.com>
2023-03-20 21:19:37 +0200

    Update users urls

--- src/newsapp/users/urls.py
+++ src/newsapp/users/urls.py
@@ -4,5 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+from users.views import UserView
 
-urlpatterns: t.List[t.Any] = []
+router = routers.DefaultRouter()
+router.register(r"users", UserView)
+
+urlpatterns: t.List[t.Any] = [
+    path("api/auth/login/", views.obtain_auth_token),
+] + router.urls
--- src/newsapp/users/views.py
+++ src/newsapp/users/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import generics, mixins, views, viewsets
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserView(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
