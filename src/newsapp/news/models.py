0bd5503a016e9b53b22c6722e18bd4c4ba006bc9
dmytromayor <mayor4090@gmail.com>
2023-03-20 21:02:51 +0200

    Add news model

--- src/newsapp/news/models.py
+++ src/newsapp/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=63,
+    )
+    content = models.TextField(
+        max_length=4095,
+    )
