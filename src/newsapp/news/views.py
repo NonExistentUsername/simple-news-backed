c2e745ecb8c2e8812d5e4a13d5ee24d6ebd8d0d8
dmytromayor <mayor4090@gmail.com>
2023-03-20 22:46:50 +0200

    Update newsview

--- src/newsapp/news/views.py
+++ src/newsapp/news/views.py
@@ -1,9 +1,11 @@
+from django.db.models import Q
 from django.shortcuts import render
 from news.models import News
 from news.serializers import NewsSerializer
-from rest_framework import mixins, views, viewsets
+from rest_framework import mixins, status, views, viewsets
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 
 class NewsView(
@@ -14,10 +16,35 @@ class NewsView(
     mixins.UpdateModelMixin,
 ):
     serializer_class = NewsSerializer
-    queryset = News.objects.all()
+    queryset = News.objects.filter(
+        is_published=True,
+        is_banned=False,
+    )
 
     authentication_classes = [SessionAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return News.objects.all()
+
+        if not self.request.user.is_anonymous:
+            return News.objects.filter((Q(is_published=True) & Q(is_banned=False)) | Q(created_by=self.request.user))
+
+        return super().get_queryset()
+
+    def update(self, request, *args, **kwargs):
+        if self.request.user.is_staff:
+            return super().update(request, *args, **kwargs)
+
+        object: News = self.get_object()
+        if self.request.user != object.created_by:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        if object.is_banned:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        return super().update(request, *args, **kwargs)
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
