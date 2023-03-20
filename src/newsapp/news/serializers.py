44c8a18a2abd669d262cfeec115bb35fc8e4c719
dmytromayor <mayor4090@gmail.com>
2023-03-20 22:04:07 +0200

    Update news view and serializer

--- src/newsapp/news/serializers.py
+++ src/newsapp/news/serializers.py
@@ -1,8 +1,24 @@
+from django.contrib.auth.models import User
 from news.models import News
 from rest_framework import serializers
 
 
 class NewsSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(
+        read_only=True,
+    )
+
     class Meta:
         model = News
-        fields = "__all__"
+        fields = (
+            "id",
+            "title",
+            "content",
+            "created_by",
+            "created_at",
+        )
+        extra_kwargs = {"created_by": {"read_only": True}}
+
+    def save(self, **kwargs):
+        self.validated_data["created_by"] = self.context["user"]
+        return super().save(**kwargs)
--- src/newsapp/news/views.py
+++ src/newsapp/news/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from news.models import News
 from news.serializers import NewsSerializer
 from rest_framework import mixins, views, viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class NewsView(
@@ -12,3 +14,11 @@ class NewsView(
 ):
     serializer_class = NewsSerializer
     queryset = News.objects.all()
+
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["user"] = self.request.user
+        return context
