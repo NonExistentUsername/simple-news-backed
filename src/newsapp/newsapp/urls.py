8931192809a0b371957defa318542fa3110fd8e4
dmytromayor <mayor4090@gmail.com>
2023-03-20 21:07:18 +0200

    Add urls for users app

--- src/newsapp/newsapp/urls.py
+++ src/newsapp/newsapp/urls.py
@@ -4,12 +4,14 @@ from django.contrib import admin
 from django.urls import include, path
 from news.urls import urlpatterns as news_urlpatterns
 from rest_framework.authtoken import views
+from users.urls import urlpatterns as users_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/news/", include(news_urlpatterns)),
     path("api/auth/login/", views.obtain_auth_token),
+    path("api/auth/", include(users_urlpatterns)),
 ]
 
 if settings.DEBUG:
---
+++ src/newsapp/users/urls.py
@@ -0,0 +1,8 @@
+import typing as t
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns: t.List[t.Any] = []
