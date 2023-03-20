b93351623bd63c88b6704b145565c7665b0a3364
dmytromayor <mayor4090@gmail.com>
2023-03-20 21:19:14 +0200

    Update user serializer

--- src/newsapp/users/serializers.py
+++ src/newsapp/users/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=128, write_only=True)
+
     class Meta:
         model = User
-        fields = "__all__"
+        fields = [
+            "username",
+            "email",
+            "password",
+        ]
