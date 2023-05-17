55c0a316af8b398079763daf7d5eb37f9b1fbc28
dmytromayor <mayor4090@gmail.com>
2023-05-17 15:30:04 +0300

    Update pagination settings

--- src/newsapp/newsapp/settings.py
+++ src/newsapp/newsapp/settings.py
@@ -85,7 +85,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 25,
+    "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "EXCEPTION_HANDLER": "newsapp.utils.custom_exception_handler",
 }
